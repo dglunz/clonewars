6a64f7734f8c94b5d6bc616749c3341783f735ee
Daniel Glunz <dannyglunz@gmail.com>
2014-11-02 14:23:50 -0700

    add structure for every page

--- lib/app.rb
+++ lib/app.rb
@@ -9,14 +9,34 @@ class TwoFistedApp < Sinatra::Base
   end
 
   not_found do
-    # erb :error
+    erb :error
   end
 
   get '/' do
     erb :home
   end
 
+  get '/phone' do
+    erb :phone
+  end
+
   get '/what' do
     erb :what
+  end
+
+  get '/when' do
+    erb :when
+  end
+
+  get '/where' do
+    erb :where
+  end
+
+  get '/why' do
+    erb :why
+  end
+
+  get '/who' do
+    erb :who
   end
 end
